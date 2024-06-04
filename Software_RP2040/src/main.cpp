b30883fa48f91f7f78bf91fbd972fe65d8779f05
Andreas <reichenauer.andreas@gmail.com>
2024-06-04 10:04:54 +0200

    add sht45 add tmp117 add other 5 amg8833 change value sending so all pixels of one camera are sent in one message

--- Software_RP2040/src/main.cpp
+++ Software_RP2040/src/main.cpp
@@ -1,40 +1,73 @@
 #include <Adafruit_AMG88xx.h>
+#include <Adafruit_TMP117.h>
 #include <Arduino.h>
 #include <PacketSerial.h>
 #include <SPI.h>
+#include <SensirionErrors.h>
+#include <SensirionI2cSht4x.h>
 #include <TCA9548.h>
 #include <Wire.h>
 
+#include <array>
+#include <string>
+
+/******************************** Defines ********************************/
+
 #define DEBUG 0
+
 #define I2C_MUX_ADDRESS 0x70
 
-Adafruit_AMG88xx amg;
-TCA9548 i2_mux(I2C_MUX_ADDRESS);
-
-PacketSerial_<COBS, 0, 512> myPacketSerial;
-
 // Type of transfer packet
-
 #define PKT_TYPE_SENSOR_IR_CAMERA_1 0XB0
+#define PKT_TYPE_SENSOR_IR_CAMERA_2 0XB1
+#define PKT_TYPE_SENSOR_IR_CAMERA_3 0XB2
+#define PKT_TYPE_SENSOR_IR_CAMERA_4 0XB3
+#define PKT_TYPE_SENSOR_IR_CAMERA_5 0XB4
+#define PKT_TYPE_SENSOR_IR_CAMERA_6 0XB5
+#define PKT_TYPE_SENSOR_SURF_TEMP 0XB6
+#define PKT_TYPE_SENSOR_AIR_TEMP 0XB7
+#define PKT_TYPE_SENSOR_AIR_HUM 0XB8
 #define PKT_TYPE_CMD_COLLECT_INTERVAL 0xA0
 #define PKT_TYPE_CMD_BEEP_ON 0xA1
 #define PKT_TYPE_CMD_SHUTDOWN 0xA3
+#define PKT_TYPE_INVALID 0xFF
 
-float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
+#define NUMBER_OF_CAMERAS 6
 
-// sensor data send to  esp32
-void sensor_data_send(uint8_t type, uint8_t index, float data) {
-  uint8_t data_buf[32] = {0};
+// size necessary for data of IR camera (64 pixels + type)
+#define TRX_BUFFER_SIZE AMG88xx_PIXEL_ARRAY_SIZE * 4 + 1
+
+/******************************* Variables *******************************/
+
+Adafruit_AMG88xx amg8833;
+TCA9548 i2_mux(I2C_MUX_ADDRESS);
+SensirionI2cSht4x sht45;
+Adafruit_TMP117 tmp117;
+
+PacketSerial myPacketSerial;
+
+bool ir_cameras_connected = false;
+bool surface_temp_connected = false;
+bool air_sensor_connected = false;
+
+/*************************** Helper Functions ****************************/
+
+// send sensor data to esp32
+void sensor_data_send(uint8_t type, const std::vector<float>& data) {
+  uint8_t data_buf[TRX_BUFFER_SIZE] = {0};
   int size = 0;
 
   data_buf[0] = type;
   size++;
 
-  data_buf[1] = index;
-  size++;
-
-  memcpy(&data_buf[2], &data, sizeof(float));
-  size += sizeof(float);
+  for (const auto& data_single : data) {
+    memcpy(&data_buf[size], &data_single, sizeof(float));
+    size += sizeof(float);
+    if (size > TRX_BUFFER_SIZE) {
+      Serial.println("Tried to send too many bytes!");
+      return;
+    }
+  }
 
   myPacketSerial.send(data_buf, size);
 #if DEBUG
@@ -71,35 +104,162 @@ void sensor_power_off(void) {
   digitalWrite(18, LOW);
 }
 
-/************************ amg8833 IR camera  ****************************/
+uint8_t get_packet_type_from_index(const int index) {
+  switch (index) {
+    case 0:
+      return PKT_TYPE_SENSOR_IR_CAMERA_1;
+    case 1:
+      return PKT_TYPE_SENSOR_IR_CAMERA_2;
+    case 2:
+      return PKT_TYPE_SENSOR_IR_CAMERA_3;
+    case 3:
+      return PKT_TYPE_SENSOR_IR_CAMERA_4;
+    case 4:
+      return PKT_TYPE_SENSOR_IR_CAMERA_5;
+    case 5:
+      return PKT_TYPE_SENSOR_IR_CAMERA_6;
+    default:
+      return PKT_TYPE_INVALID;
+  }
+}
+
+/*************************** AMG8833 IR camera ***************************/
+using pixel_values = std::array<float, AMG88xx_PIXEL_ARRAY_SIZE>;
+std::array<pixel_values, 6> pixels_all;
 
 void sensor_amg8833_init(void) {
-  Serial.println(F("AMG88xx thermal camera!"));
+#if DEBUG
+  Serial.println(F("AMG88xx Thermal Camera!"));
+#endif
 
-  bool status;
-
-  status = amg.begin();
-  if (!status) {
+  if (!amg8833.begin()) {
     Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
-    while (1);
+    ir_cameras_connected = false;
+    return;
   }
 
-  Serial.println("-- Thermal Camera Test --");
+  ir_cameras_connected = true;
+  Serial.println("-- Thermal Camera Connected --");
 }
 
-void sensor_amg8833_get(void) {
-  amg.readPixels(pixels);
-
-  for (int index = 0; index < AMG88xx_PIXEL_ARRAY_SIZE; index++) {
-    Serial.printf("AMG8833 Pixel %d: ", index);
-    Serial.println(pixels[index]);
-
-    sensor_data_send(PKT_TYPE_SENSOR_IR_CAMERA_1, static_cast<uint8_t>(index),
-                     static_cast<float>(pixels[index]));
+void sensor_amg8833_read(const int camera_index) {
+  const uint8_t paket_type = get_packet_type_from_index(camera_index);
+  if (paket_type == PKT_TYPE_INVALID) {
+    Serial.println("Invalid Paket Type!");
+    return;
   }
+
+  amg8833.readPixels(pixels_all[camera_index].data());
+
+#if DEBUG
+  for (int pixel_index = 0; pixel_index < AMG88xx_PIXEL_ARRAY_SIZE;
+       pixel_index++) {
+    Serial.printf("AMG8833 %d Pixel %d: ", camera_index, pixel_index);
+    Serial.println(pixels_all[camera_index][pixel_index]);
+  }
+#endif
+
+  sensor_data_send(paket_type,
+                   std::vector<float>(pixels_all[camera_index].begin(),
+                                      pixels_all[camera_index].end()));
 }
 
-/************************ beep ****************************/
+/******************* TMP117 Surface Temperature Sensor *******************/
+void sensor_tmp117_init(void) {
+#if DEBUG
+  Serial.println(F("TMP117 Temperature Sensor!"));
+#endif
+
+  if (!tmp117.begin()) {
+    Serial.println("Could not find a valid TMP117 sensor, check wiring!");
+    surface_temp_connected = false;
+    return;
+  }
+
+  surface_temp_connected = true;
+  Serial.println("-- Surface Temperature Sensor Connected --");
+}
+
+void sensor_tmp117_read(void) {
+  // create an empty event to be filled
+  sensors_event_t temp_event;
+  // fill the empty event object with the current measurements
+  tmp117.getEvent(&temp_event);
+
+#if DEBUG
+  Serial.print("TMP117 Temperature: ");
+  Serial.println(temp_event.temperature);
+#endif
+
+  sensor_data_send(PKT_TYPE_SENSOR_SURF_TEMP, {temp_event.temperature});
+}
+
+/*********************** SHT45 Air T and rH Sensor ***********************/
+char error_message[64];
+int16_t error;
+
+void sensor_sht45_init(void) {
+#if DEBUG
+  Serial.println(F("SHT45 Temperature and Humidity Sensor!"));
+#endif
+
+  sht45.begin(Wire, SHT40_I2C_ADDR_44);
+
+  error = sht45.softReset();
+
+  if (error != NoError) {
+    Serial.print("Error trying to do soft reset: ");
+    errorToString(error, error_message, sizeof error_message);
+    Serial.println(error_message);
+    air_sensor_connected = false;
+    return;
+  }
+  delay(10);
+
+  uint32_t serialNumber = 0;
+  error = sht45.serialNumber(serialNumber);
+  if (error != NoError) {
+    Serial.print("Error trying to read serial number: ");
+    errorToString(error, error_message, sizeof(error_message));
+    Serial.println(error_message);
+    air_sensor_connected = false;
+    return;
+  }
+
+#if DEBUG
+  Serial.print("serialNumber: ");
+  Serial.print(serialNumber);
+  Serial.println();
+#endif
+
+  air_sensor_connected = true;
+  Serial.println("-- Air Temperature and Humidity Sensor Connected --");
+}
+
+void sensor_sht45_read(void) {
+  float temperature = 0.0;
+  float humidity = 0.0;
+  delay(20);
+  error = sht45.measureHighPrecision(temperature, humidity);
+  if (error != NoError) {
+    Serial.print("Error trying to read sensor data: ");
+    errorToString(error, error_message, sizeof(error_message));
+    Serial.println(error_message);
+    return;
+  }
+
+#if DEBUG
+  Serial.print("SHT45 Temperature: ");
+  Serial.print(temperature);
+  Serial.print(", Humidity: ");
+  Serial.println(humidity);
+#endif
+
+  sensor_data_send(PKT_TYPE_SENSOR_AIR_TEMP, {temperature});
+  sensor_data_send(PKT_TYPE_SENSOR_AIR_HUM, {humidity});
+}
+
+/********************************* beep **********************************/
 
 #define Buzzer 19  // Buzzer GPIO
 
@@ -111,11 +271,11 @@ void beep_on(void) {
   analogWrite(Buzzer, 0);
 }
 
-/************************ recv cmd from esp32  ****************************/
+/************************** recv cmd from esp32 **************************/
 
 static bool shutdown_flag = false;
 
-void onPacketReceived(const uint8_t *buffer, size_t size) {
+void onPacketReceived(const uint8_t* buffer, size_t size) {
 #if DEBUG
   Serial.printf("<--- recv len:%d, data: ", size);
   for (int i = 0; i < size; i++) {
@@ -138,7 +298,7 @@ void onPacketReceived(const uint8_t *buffer, size_t size) {
   }
 }
 
-/************************ setuo & loop ****************************/
+/***************************** setup & loop ******************************/
 
 int i = 0;
 
@@ -160,8 +320,13 @@ void setup() {
     Serial.println("Could not connect to I2C Multiplexer!");
   }
 
-  i2_mux.selectChannel(2);
-  sensor_amg8833_init();
+  // init all IR cameras
+  for (unsigned int index = 0; index < NUMBER_OF_CAMERAS; ++index) {
+    i2_mux.selectChannel(index);
+    sensor_amg8833_init();
+  }
+  sensor_tmp117_init();
+  sensor_sht45_init();
 
   beep_init();
   delay(500);
@@ -171,11 +336,15 @@ void setup() {
 void loop() {
   if (i > 500) {
     i = 0;
-
-    Serial.print("\r\n\r\n--------- start measure -------\r\n");
-
-    i2_mux.selectChannel(2);
-    sensor_amg8833_get();
+    // read from all IR cameras
+    if (ir_cameras_connected)
+      for (unsigned int index = 0; index < NUMBER_OF_CAMERAS; ++index) {
+        i2_mux.selectChannel(index);
+        sensor_amg8833_read(index);
+        delay(50);
+      }
+    if (surface_temp_connected) sensor_tmp117_read();
+    if (air_sensor_connected) sensor_sht45_read();
   }
 
   i++;
