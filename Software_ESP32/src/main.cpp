8f832f84ccdb68ffb7d1622ece15948eb852b2fd
Andreas <reichenauer.andreas@gmail.com>
2024-06-04 10:06:03 +0200

    add message handling for all remaining sensors and change handling of ir cameras to read all pixels from message

--- Software_ESP32/src/main.cpp
+++ Software_ESP32/src/main.cpp
@@ -2,12 +2,25 @@
 #include <PCA95x5.h>
 #include <PacketSerial.h>
 
-PacketSerial_<COBS, 0, 512> myPacketSerial;
+#include <numeric>
+
+#include "measurement_data.h"
+
+#define DEBUG 0
 
 #define RXD2 20
 #define TXD2 19
 
+// Type of transfer packet
 #define PKT_TYPE_SENSOR_IR_CAMERA_1 0XB0
+#define PKT_TYPE_SENSOR_IR_CAMERA_2 0XB1
+#define PKT_TYPE_SENSOR_IR_CAMERA_3 0XB2
+#define PKT_TYPE_SENSOR_IR_CAMERA_4 0XB3
+#define PKT_TYPE_SENSOR_IR_CAMERA_5 0XB4
+#define PKT_TYPE_SENSOR_IR_CAMERA_6 0XB5
+#define PKT_TYPE_SENSOR_SURF_TEMP 0XB6
+#define PKT_TYPE_SENSOR_AIR_TEMP 0XB7
+#define PKT_TYPE_SENSOR_AIR_HUM 0XB8
 
 #define GFX_BL \
   DF_GFX_BL  // default backlight pin, you may replace DF_GFX_BL to actual
@@ -48,6 +61,9 @@ Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
  * End of Arduino_GFX setting
  ******************************************************************************/
 
+PacketSerial_<COBS, 0, 512> myPacketSerial;
+measurement_data data;
+
 void onPacketReceived(const uint8_t *buffer, size_t size);
 
 void setup(void) {
@@ -75,7 +91,6 @@ void setup(void) {
   gfx->setTextColor(RED);
   gfx->println("Sensecap Indicator");
 
-  // myPacketSerial.begin(115200);
   Serial1.begin(115200, SERIAL_8N1, RXD2, TXD2);
   myPacketSerial.setStream(&Serial1);
   myPacketSerial.setPacketHandler(&onPacketReceived);
@@ -83,38 +98,105 @@ void setup(void) {
   delay(5000);  // 5 seconds
 }
 
-void loop() {
-  gfx->setCursor(random(gfx->width()), random(gfx->height()));
-  gfx->setTextColor(random(0xffff), random(0xffff));
-  gfx->setTextSize(random(6) /* x scale */, random(6) /* y scale */,
-                   random(2) /* pixel_margin */);
-  gfx->println("Sensecap Indicator");
-  delay(1000);  // 1 second
+int count = 0;
 
+void loop() {
   myPacketSerial.update();
   if (myPacketSerial.overflow()) {
     Serial.println("Buffer Overflow");
   }
+
+  if (count >= 100) {
+    count = 0;
+
+#if DEBUG
+    data.print_to_serial(false);
+#endif
+
+    gfx->setCursor(random(gfx->width()), random(gfx->height()));
+    gfx->setTextColor(random(0xffff), random(0xffff));
+    gfx->setTextSize(random(6) /* x scale */, random(6) /* y scale */,
+                     random(2) /* pixel_margin */);
+    gfx->println("Sensecap Indicator");
+  }
+
+  ++count;
+  delay(10);  // 10 milliseconds
+}
+
+void handle_camera_received(const uint8_t *buffer, const int camera_index,
+                            const size_t &size) {
+  size_t size_converted = 1;
+  unsigned int pixel_index = 0;
+  while (size_converted < size) {
+    float dataval;
+    memcpy(&dataval, &buffer[size_converted], sizeof(float));
+    data.ir_camera_data[camera_index][pixel_index] = dataval;
+    size_converted += sizeof(float);
+    ++pixel_index;
+  }
+  if (pixel_index != PIXEL_COUNT) {
+    Serial.println("Not all pixels were sent!");
+    return;
+  }
+  data.ir_camera_means[camera_index] =
+      std::accumulate(data.ir_camera_data[camera_index].cbegin(),
+                      data.ir_camera_data[camera_index].cend(), 0.0) /
+      PIXEL_COUNT;
 }
 
 void onPacketReceived(const uint8_t *buffer, size_t size) {
-  Serial.printf("<--- recv len:%d, data: ", size);
-
   if (size < 1) {
     return;
   }
-  // byte serbytes[] = buffer[i];
-  float dataval;
-  uint8_t index;
   switch (buffer[0]) {
     case PKT_TYPE_SENSOR_IR_CAMERA_1: {
-      index = buffer[1];
-      memcpy(&dataval, &buffer[2], sizeof(float));
-      Serial.printf("Camera 1 Result, Pixel %d: ", index);
-      Serial.println(dataval);
+      handle_camera_received(buffer, 0, size);
+      break;
+    }
+    case PKT_TYPE_SENSOR_IR_CAMERA_2: {
+      handle_camera_received(buffer, 1, size);
+      break;
+    }
+    case PKT_TYPE_SENSOR_IR_CAMERA_3: {
+      handle_camera_received(buffer, 2, size);
+      break;
+    }
+    case PKT_TYPE_SENSOR_IR_CAMERA_4: {
+      handle_camera_received(buffer, 3, size);
+      break;
+    }
+    case PKT_TYPE_SENSOR_IR_CAMERA_5: {
+      handle_camera_received(buffer, 4, size);
+      break;
+    }
+    case PKT_TYPE_SENSOR_IR_CAMERA_6: {
+      handle_camera_received(buffer, 5, size);
+      break;
+    }
+    case PKT_TYPE_SENSOR_SURF_TEMP: {
+      float dataval;
+      // index only necessary for IR cameras, skipped
+      memcpy(&dataval, &buffer[1], sizeof(float));
+      data.surface_temp = dataval;
+      break;
+    }
+    case PKT_TYPE_SENSOR_AIR_TEMP: {
+      float dataval;
+      // index only necessary for IR cameras, skipped
+      memcpy(&dataval, &buffer[1], sizeof(float));
+      data.air_temp = dataval;
+      break;
+    }
+    case PKT_TYPE_SENSOR_AIR_HUM: {
+      float dataval;
+      // index only necessary for IR cameras, skipped
+      memcpy(&dataval, &buffer[1], sizeof(float));
+      data.air_rH = dataval;
       break;
     }
     default:
+      Serial.println("Invalid Paket received!");
       break;
   }
 }
